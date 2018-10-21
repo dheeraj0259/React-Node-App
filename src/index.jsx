c4b4a10df0ca3146c28c1f70fef2a56340625d9a
dheeraj0259 <dheeeraj0259@gmail.com>
2018-10-20 21:05:43 -0500

    refactored code

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import routes from './routes';
 
 import HelloWorld from './components/HelloWorld'
 
 import './styles/app.scss';
-
+//shows compilation error
 ReactDom.render(
-  <HelloWorld />,
+  <Router history={browserHistory} routes={routes} />,
   document.getElementById('app')
 )
