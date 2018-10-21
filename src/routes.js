6e1d72ab2d7dfe8db41c91583f0dca54fb5d0d33
dheeraj0259 <dheeeraj0259@gmail.com>
2018-10-20 21:35:40 -0500

    added routes.js file, gitignore

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import HelloWorld from './components/HelloWorld';
+
+export default (
+  <Route path="/" component={HelloWorld}>
+  </Route>
+);
